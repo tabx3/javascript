6e3f2010d0613630a316652f2746767cb5273d4b
tabx <tabx@tabxdeMac-mini.lan>
2025-03-10 15:11:25 +0800

    usereducer

---
+++ react/react-reducer/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { useReducer } from "react";
+const initialState = { count: 0 };
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <div>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+    </div>
+  );
+}
